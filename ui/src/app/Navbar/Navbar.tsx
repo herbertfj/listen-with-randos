84f4e9e8f68b76371200ec9ad9a1fa82b4a0fa9d
Herbert Freyre <herbertfj@gmail.com>
2018-11-05 18:03:16 -0500

    Display logged in user display name

--- ui/src/app/Navbar/Navbar.tsx
+++ ui/src/app/Navbar/Navbar.tsx
@@ -3,15 +3,21 @@ import { Link } from "react-router-dom"
 import { LoginLink } from "../LoginLink/LoginLink"
 import { LoggedInRender, LoggedOutRender } from "../LoginSwitch/LoginSwitch"
 import { LogoutButton } from "./LogoutButton/LogoutButton"
+import { DisplayName } from "../DisplayName/DisplayName"
 
 export const Navbar: React.SFC = () => (
-  <nav className="navbar">
+  <nav className="navbar navbar-expand">
     <div className="container">
       <Link to="/" className="navbar-brand">
         Listen With Randos
       </Link>
       <ul className="navbar-nav">
         <LoggedInRender>
+          <li className="nav-item">
+            <div className="navbar-text">
+              <DisplayName />
+            </div>
+          </li>
           <li className="nav-item">
             <LogoutButton />
           </li>
