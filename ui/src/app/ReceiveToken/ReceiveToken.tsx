4e66944a21d2e9cca5f1ce3bc9eebd130f2de0b7
Herbert Freyre <herbertfj@gmail.com>
2018-11-08 17:12:29 -0500

    Replace instead of pushing state on receiving token

--- ui/src/app/ReceiveToken/ReceiveToken.tsx
+++ ui/src/app/ReceiveToken/ReceiveToken.tsx
@@ -8,7 +8,7 @@ import { AppAction } from "../../domain/root"
 import { parse } from "qs"
 
 type ReceiveTokenDispatchProps = {
-  keepToken: (accessToken: string) => void
+  keepToken(accessToken: string): void
 }
 
 type ReceiveTokenProps = RouteComponentProps & ReceiveTokenDispatchProps
@@ -17,8 +17,8 @@ const ReceiveToken: React.SFC<ReceiveTokenProps> = props => {
   useEffect(() => {
     const hashParams = props.location.hash.substring(1)
     props.keepToken(parse(hashParams).access_token)
-    props.history.push("/")
-  })
+    props.history.replace("/")
+  }, [])
 
   return null
 }
