3714695a34b533a83ac6c977bf97c89b67c1a6fe
Herbert Freyre <herbertfj@gmail.com>
2019-03-18 00:17:21 -0400

    Rename chats core module

--- components/chats-api/build.gradle.kts
+++ components/chats-api/build.gradle.kts
@@ -1,5 +1,5 @@
 dependencies {
-    implementation(project(":components:chats"))
+    implementation(project(":components:chats-core"))
     implementation(project(":components:users"))
     implementation(project(":components:mongo-chats"))
     implementation("org.springframework.boot:spring-boot-starter-webflux")
--- components/mongo-chats/build.gradle.kts
+++ components/mongo-chats/build.gradle.kts
@@ -1,5 +1,5 @@
 dependencies {
-    implementation(project(":components:chats"))
+    implementation(project(":components:chats-core"))
 
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
 }
